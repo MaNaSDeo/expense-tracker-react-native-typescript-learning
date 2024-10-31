da4f86c2dae84c87543e51c2f195474c345c5652
manasdeo <rmnslogin@gmail.com>
2024-10-31 23:09:42 +0530

    starting setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import RecentExpenses from "./Screens/RecentExpenses";
+import AllExpenses from "./Screens/AllExpenses";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Recent" component={RecentExpenses} />
+          <Tab.Screen name="All Expenses" component={AllExpenses} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
